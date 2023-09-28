3ec56e5dfb11e293e3e065f4ce6153ac59600984
Hayley <haysho2260@gmail.com>
2023-09-27 17:12:31 -0700

    Make red_bottles not hardcoded in catalog.py access db and use sql to get info

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+import sqlalchemy
+from src import database as db
 
 router = APIRouter()
 
@@ -9,13 +11,23 @@ def get_catalog():
     Each unique item combination must have only a single price.
     """
 
+    # The sql we want to execute
+    sql = """
+    SELECT num_red_potions FROM global_inventory
+    """
+    with db.engine.begin() as connection:
+        # Run the sql and returns a CursorResult object which represents the SQL results
+        # Execute SQL statement to get num of red potions
+        result = connection.execute(sqlalchemy.text(sql))
+        first_row = result.fetchone()
+        num_red_potions_to_sell = first_row.num_red_potions()    
+    
     # Can return a max of 20 items.
-
     return [
             {
                 "sku": "RED_POTION_0",
                 "name": "red potion",
-                "quantity": 1,
+                "quantity": num_red_potions_to_sell,
                 "price": 50,
                 "potion_type": [100, 0, 0, 0],
             }
