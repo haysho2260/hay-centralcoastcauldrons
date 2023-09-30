9f471a5be6195cac3e4a238b5a42799fda7766c9
Hayley <haysho2260@gmail.com>
2023-09-30 14:01:47 -0700

    remove test comment

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -39,7 +39,6 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
         
     print(barrels_delivered)
     return "OK"
-    # return ["HELLO", num_red_ml_have, num_gold_have]
 
 # Gets called once a day
 @router.post("/plan")
