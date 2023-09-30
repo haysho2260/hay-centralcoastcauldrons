6bfce4f521248bf5d31f474d48d2bdb19f3382b0
Hayley <haysho2260@gmail.com>
2023-09-30 14:30:54 -0700

    updaate shop infor and reset func

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/admin",
@@ -14,16 +16,20 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
+    # TODO: reset barrels from inventory, reset carts
+    with db.engine.begin() as connection:
+        # update
+        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_red_ml = 0"))
+        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_red_potions = 0"))
+        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold = 100"))
     return "OK"
 
 
 @router.get("/shop_info/")
 def get_shop_info():
-    """ """
 
-    # TODO: Change me!
     return {
-        "shop_name": "Potion Shop",
-        "shop_owner": "Potion Seller",
+        "shop_name": "SHOP HERE!",
+        "shop_owner": "Hayley Chang",
     }
 
