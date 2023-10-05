e00014fb3bde92cc6f355d08892a2a1da69e3838
Hayley <haysho2260@gmail.com>
2023-10-05 12:51:02 -0700

    update catalog to have green and blue

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -10,29 +10,67 @@ def get_catalog():
     """
     Each unique item combination must have only a single price.
     """
-
+    catalog = []
     # The sql we want to execute
     sql = """
-    SELECT num_red_potions FROM global_inventory
+    SELECT num_red_potions, num_green_potions, num_blue_potions FROM global_inventory
     """
     with db.engine.begin() as connection:
         # Run the sql and returns a CursorResult object which represents the SQL results
         # Execute SQL statement to get num of red potions
         result = connection.execute(sqlalchemy.text(sql))
         first_row = result.first()
-        num_red_potions_to_sell = first_row.num_red_potions 
-        print(f"get_catalog: num_red_potions_to_sell {num_red_potions_to_sell}")
-    
-    # Can return a max of 20 items.
-    if num_red_potions_to_sell > 0:
-        return [
-                {
-                    "sku": "RED_POTION_0",
+        if (num_red_potions_to_sell := first_row.num_red_potions) > 0:
+            catalog.append({
+                    "sku": "RED_POTION_100",
                     "name": "red potion",
                     "quantity": num_red_potions_to_sell,
                     "price": 50,
                     "potion_type": [100, 0, 0, 0],
-                }
-            ]
-    else: 
-        return []
+                })
+        if (num_green_potions_to_sell := first_row.num_green_potions) > 0:
+            catalog.append({
+                    "sku": "GREEN_POTION_100",
+                    "name": "green potion",
+                    "quantity": num_green_potions_to_sell,
+                    "price": 50,
+                    "potion_type": [0, 100, 0, 0],
+                })
+        if (num_blue_potions_to_sell := first_row.num_blue_potions) > 0:
+            catalog.append({
+                    "sku": "BLUE_POTION_100",
+                    "name": "blue potion",
+                    "quantity": num_blue_potions_to_sell,
+                    "price": 50,
+                    "potion_type": [0, 0, 100, 0],
+                })
+        print(f"get_catalog: num_red_potions_to_sell {num_red_potions_to_sell}")
+        print(f"get_catalog: num_green_potions_to_sell {num_green_potions_to_sell}")
+        print(f"get_catalog: num_blue_potions_to_sell {num_blue_potions_to_sell}")
+    
+    # Can return a max of 20 items.
+    # if num_red_potions_to_sell > 0:
+    #     return [
+    #             {
+    #                 "sku": "RED_POTION_100",
+    #                 "name": "red potion",
+    #                 "quantity": num_red_potions_to_sell,
+    #                 "price": 50,
+    #                 "potion_type": [100, 0, 0, 0],
+    #             },
+    #             {
+    #                 "sku": "GREEN_POTION_100",
+    #                 "name": "green potion",
+    #                 "quantity": num_green_potions_to_sell,
+    #                 "price": 50,
+    #                 "potion_type": [0, 100, 0, 0],
+    #             },
+    #             {
+    #                 "sku": "BLUE_POTION_100",
+    #                 "name": "blue potion",
+    #                 "quantity": num_blue_potions_to_sell,
+    #                 "price": 50,
+    #                 "potion_type": [0, 0, 100, 0],
+    #             }
+    #         ]
+    return catalog
