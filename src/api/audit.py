5e5f54d1b1f7652bc3e5ae7356ff23a3285223aa
Hayley <haysho2260@gmail.com>
2023-10-06 14:01:31 -0700

    update audit to include other colors

--- src/api/audit.py
+++ src/api/audit.py
@@ -4,6 +4,8 @@ from src.api import auth
 import math
 import sqlalchemy
 from src import database as db
+from src.api.temp_dict import colors
+from fastapi import HTTPException
 
 router = APIRouter(
     prefix="/audit",
@@ -13,18 +15,33 @@ router = APIRouter(
 
 @router.get("/inventory")
 def get_inventory():
-    sql = """
-    SELECT num_red_potions, num_red_ml, gold FROM global_inventory
-    """
-    with db.engine.begin() as connection:
-        # Run the sql and returns a CursorResult object which represents the SQL results
-        # Execute SQL statement to get num of red potions
-        result = connection.execute(sqlalchemy.text(sql))
-        first_row = result.fetchone()
-        num_red_potions_to_sell = first_row.num_red_potions
-        num_red_ml = first_row.num_red_ml
-        gold = first_row.gold
-    return {"number_of_potions": num_red_potions_to_sell, "ml_in_barrels": num_red_ml, "gold": gold}
+    total_potions = 0
+    total_ml = 0
+    for color in colors:
+            # Define the SQL query with placeholders for color
+            sql = """
+                SELECT num_{}_ml, num_{}_potions FROM global_inventory
+            """
+
+            # Replace the placeholders with the actual color value
+            formatted_sql = sql.format(color, color)
+
+            # Execute the query
+            # see how much num_ml & num_potions we have
+            with db.engine.begin() as connection:
+                num_ml_have, num_potions_have = connection.execute(sqlalchemy.text(formatted_sql)).first()
+            
+                print(f"get_inventory: potion num_{color}_ml_have {num_ml_have}")
+                print(f"get_inventory: potion num_{color}_potions_have {num_potions_have}")
+                
+                total_potions += num_potions_have
+                total_ml += num_ml_have
+    
+    print(f"get_inventory: total_potions {total_potions}")
+    print(f"get_inventory: total_ml {total_ml}")       
+    gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).scalar()
+    print(f"get_inventory: gold {gold}")
+    return {"number_of_potions": total_potions, "ml_in_barrels": total_ml, "gold": gold}
 
 class Result(BaseModel):
     gold_match: bool
