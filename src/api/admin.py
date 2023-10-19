cabb7721cdf1f6dbb06306287e6311b8c869f93e
Hayley <haysho2260@gmail.com>
2023-10-19 12:41:20 -0700

    delete quantity to add to potions inventory

--- src/api/admin.py
+++ src/api/admin.py
@@ -24,11 +24,11 @@ def reset():
         connection.execute(sqlalchemy.text("""
             -- Insert the initial rows
             INSERT INTO public.potions_catalog (sku, quantity, price)
-            VALUES ('50_50_0_0', 0, 1),
-                ('0_50_50_0', 0, 1),
-                ('50_0_50_0', 0, 1),
-                ('100_0_0_0', 0, 50),
-                ('0_100_0_0', 0, 50);
+            VALUES ('50_50_0_0', 1),
+                ('0_50_50_0', 1),
+                ('50_0_50_0', 1),
+                ('100_0_0_0', 50),
+                ('0_100_0_0', 50),
         """))
         connection.execute(sqlalchemy.text("""
             -- Insert the initial rows for global
