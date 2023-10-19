8c1908200b6cccd8870aba5e026c0e7638bf854e
Hayley <haysho2260@gmail.com>
2023-10-19 12:47:52 -0700

    get values from join inventory and catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -24,18 +24,19 @@ def get_catalog():
     # Execute the select statement and fetch all rows
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("""
-            SELECT sku, SUM(quantity) AS sum_quantity, price
-            FROM potions_catalog
-            GROUP BY sku, price
+            SELECT pi.sku, pc.price, SUM(pi.quantity) AS sum_quantity
+            FROM potions_inventory AS pi
+            JOIN potions_catalog AS pc ON pi.sku = pc.sku
+            GROUP BY pi.sku, pc.price;
         """))
         rows = result.fetchall()
         for row in rows:
-            if row.sum_quantity > 0:
+             if row['sum_quantity'] > 0:
                 catalog.append({
-                    "sku": row.sku,
-                    "name": row.sku,
-                    "quantity": row.sum_quantity,
-                    "price": row.price,
-                    "potion_type": sku_to_potion(row.sku)
+                    "sku": row['sku'],
+                    "name": row['sku'],  # You can adjust 'name' as needed
+                    "quantity": row['sum_quantity'],
+                    "price": row['price'],
+                    "potion_type": sku_to_potion(row['sku'])
                 })
     return catalog
