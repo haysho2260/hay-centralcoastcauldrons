867189374bbb3bd5b1593d53ac38113dd11b2a07
Hayley <haysho2260@gmail.com>
2023-10-30 07:29:36 -0700

    fix previous start num

--- src/api/carts.py
+++ src/api/carts.py
@@ -107,7 +107,7 @@ def search_orders(
             })
 
     return {
-        "previous": int(search_page) - 5 if int(search_page) >= 5 else "",
+        "previous": int(search_page) - 5 if int(search_page) >= 0 else "",
         "next": int(search_page) + 5 if len(result) > 5 else "",
         "results": results,
     }
